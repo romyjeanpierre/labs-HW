ef68a045c92d235933aa697c7fef9447b7f9ae43
Romy <romyjeanpierre@yahoo.com>
2023-07-09 19:14:49 -0400

    Methods Revisited answered

--- answers.js
+++ answers.js
@@ -80,3 +80,57 @@ for (let item of ninjaTurtles){
 }
 console.log(ninjaTurtles1);
 
+///////////////////////////////////////////////////////////////////////
+//Methods, Revisited
+///////////////////////////////////////////////////////////////////////
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+//Console log: the index of Titanic
+console.log(favMovies[8]);
+
+//use the .sort method 
+favMovies.sort(); 
+console.log(favMovies);
+
+//Use the method pop.
+favMovies.pop()
+console.log(favMovies);
+
+//push "Guardians of the Galaxy".
+favMovies.push("Guardians of the Galaxy")
+console.log(favMovies); 
+
+//Reverse the array.
+const reversedFavMovies = favMovies.reverse();
+console.log(reversedFavMovies);
+
+//Use the shift method.
+favMovies.shift(); 
+console.log(favMovies); 
+
+
+//unshift - what does it return?
+favMovies.unshift('Bad Boys'); 
+console.log(favMovies);
+
+//splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained" instead of counting it yourself).
+favMovies.splice(15, 1, "Avatar"); 
+console.log(favMovies);
+
+
+//slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
+//Store the value of your slice in a variable and console.log it.
+//console.log your final results.
+const middleIndex = Math.ceil(favMovies.length / 2); 
+console.log(middleIndex);
+const lastHalf = favMovies.slice(-middleIndex);
+console.log(lastHalf);
+
+//store the value of your slice in a variable, console.log
+let lastHalf1 = ['Imitation of Life', "Howl's Moving Castle", 'Harry Potter', 'Harry Potter', 'Fried Green Tomatoes', 'Fast and Furious', 'Avatar', 'Conan the Barbarian', 'Cloud Atlas', 'Black Panther'];
+
+console.log (lastHalf1);
+//After running the above tasks, console.log the index of "Fast and Furious" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?
+console.log(lastHalf1[5]);
+
+
+
