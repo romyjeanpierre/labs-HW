9e04ef0aafdcdecdfd6834b5d8c76a4dde5e86ef
Romy <romyjeanpierre@yahoo.com>
2023-07-09 17:30:29 -0400

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -2,16 +2,16 @@
 //2 - I am shakey, I could use a refresher
 
 //*Part II: JavaScript Reps
-////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 //Easy Going
-///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 //Write a for loop that will log the numbers 1 through 20.
 for(let counter = 1; counter <=20; counter++) {
     console.log(counter);
 }
-/////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 //Get Even
-/////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 //Write a for loop that will log only the even numbers in 0 through 200.
 for (let i = 0; i <= 200; i += 2) {
     console.log(i);
@@ -37,3 +37,31 @@ for (let a = 1; a <= 100; a++) {
       console.log("Buzz");  
     }
 }
+
+///////////////////////////////////////////////////////////////////////
+//Wild Wild Life
+///////////////////////////////////////////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+//Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee.splice(2, 1, "5001"); 
+console.log(plantee);
+
+//Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+
+//Give D'Art a second hometown by adding "Hawkins"
+dart.push("Downside Up");
+console.log(dart);
+
+//Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+wolfy.shift();
+console.log(wolfy); 
+
+wolfy.unshift("Gameboy");
+console.log(wolfy);
