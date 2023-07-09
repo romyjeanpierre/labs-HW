95f08c56722f5957d8f11d30f638a59def705f1f
Romy <romyjeanpierre@yahoo.com>
2023-07-09 16:52:18 -0400

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -16,3 +16,24 @@ for(let counter = 1; counter <=20; counter++) {
 for (let i = 0; i <= 200; i += 2) {
     console.log(i);
 } 
+
+//////////////////////////////////////////////////////////////////////
+//Fizz Buzz
+//////////////////////////////////////////////////////////////////////
+/*Write a javascript application that logs all numbers from 1 - 100.
+If a number is divisible by 3 log "Fizz" instead of the number.
+If a number is divisible by 5 log "Buzz" instead of the number.
+If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.*/
+
+let a = 1; 
+
+for (let a = 1; a <= 100; a++) {
+    if (a % 3 && a % 5){
+        console.log("FizzBuzz");
+    } else if (a % 3) {
+        console.log("Fizz");
+    }
+    else if( a % 5) {
+      console.log("Buzz");  
+    }
+}
