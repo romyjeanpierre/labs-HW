0e2d9d69f58224fd82ea8a7ee0d7f534442e1414
Romy <romyjeanpierre@yahoo.com>
2023-07-09 20:20:44 -0400

    Where is Waldo answered

--- answers.js
+++ answers.js
@@ -132,5 +132,37 @@ console.log (lastHalf1);
 //After running the above tasks, console.log the index of "Fast and Furious" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?
 console.log(lastHalf1[5]);
 
+///////////////////////////////////////////////////////////////////////
+//Where is Waldo
+///////////////////////////////////////////////////////////////////////
+const whereIsWaldo = [
+    ["Timmy", "Frank"],
+ "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", 
+                    ["Baked Goods", "Waldo"]
+                ]
+            ];
+//Remove Eggbert (hint look at the slice/splice method(s))
+whereIsWaldo.splice(1, 1);
+console.log(whereIsWaldo)
+
+//Change "Neff" to "No One"
+whereIsWaldo[1][2] = "No One"; 
+console.log(whereIsWaldo); 
+
+//Access and console.log "Waldo"
+console.log(whereIsWaldo[2][1][1]);
+
+
+
+
+
+
+
+
+
+
+
 
 
