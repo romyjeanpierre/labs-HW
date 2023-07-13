21be5dcf3c2768eac76cc112ff08b8ee980987e9
Romy <romyjeanpierre@yahoo.com>
2023-07-13 06:18:58 -0400

    created hamster class

--- answers.js
+++ answers.js
@@ -180,6 +180,41 @@ const median = arr => {
 };
 console.log(median([14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12])); 
 
+///////////////////////////////////////////////////////////////////////
+//Hungry for More?
+///////////////////////////////////////////////////////////////////////
+
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
 
 
 
