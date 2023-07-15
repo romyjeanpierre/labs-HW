6145b5d7a433d3b76cf7ffe6fc6927884cded6ba
Romy <romyjeanpierre@yahoo.com>
2023-07-14 20:53:45 -0400

    DOM Menu Lab - Part 2

--- DOM-menu-lab/script.js
+++ DOM-menu-lab/script.js
@@ -1,10 +1,21 @@
 
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
 //task 1.0
 const mainEl = document.querySelector('main');
@@ -32,3 +43,36 @@ for(let i = 0; i < menuLinks.length; i++) {
     a.textContent = menuLinks[i].text
     topMenuEl.appendChild(a)
 }
+//task 4.0
+const subMenuEl = document.querySelector('#sub-menu');
+console.log(subMenuEl);
+//task 4.1
+subMenuEl.style.height = '100%';
+//task 4.2
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)'; 
+//task 4.3
+subMenuEl.classList.add('flex-around');
+//task 4.4
+subMenuEl.style.position = 'absolute'; 
+//task 4.5
+subMenuEl.style.top = '0';
+//task 5.1
+const topMenuLinks = document.querySelectorAll('nav#top-menu > a');
+const showingSubMenu = 'false'
+console.log(topMenuLinks);
+//task 5.2
+topMenuEl.addEventListener('click', function(a) {
+  a.preventDefault(); 
+  console.log(a);
+})
+
+
+
+
+// Delegating the UL as the element to listen for a click event
+
+//const ul = document.querySelector('ul');
+
+/*ul.addEventListener('click', function(e) {
+  e.preventDefault(); // stops a page from refreshing
+  console.log(e);*/
