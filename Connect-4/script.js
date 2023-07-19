9106193c5d483c868df9e537843973e0afacfac9
Romy <romyjeanpierre@yahoo.com>
2023-07-18 22:50:30 -0400

    day2

---
+++ Connect-4/script.js
@@ -0,0 +1,27 @@
+const player1 = 'Red'; 
+const player2 = 'Yellow'; 
+
+//first player will be the red player 
+const firstPlayer = player1
+
+const gameOver = false; 
+
+//the board will have 6 rows and 7 columns 
+let board;
+const rows = 6; 
+const columns = 7;
+
+// window.onload method gets executed after the web page is loaded including all the elements related to the DOM 
+window.onload = function() {
+    setGame()
+}
+
+function setGame() {
+    board [];
+
+    for (let r= 0; r < rows; r++){
+        let row = []; 
+
+        for (let )
+    }
+}
