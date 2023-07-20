f8ce8032af56685b1c3465f810ca1e6779a2ab87
Romy <romyjeanpierre@yahoo.com>
2023-07-19 22:06:47 -0400

    day 3

--- Connect-4/script.js
+++ Connect-4/script.js
@@ -1,27 +1,84 @@
-const player1 = 'Red'; 
-const player2 = 'Yellow'; 
+clm1 = 1; 
+clm2 = 1;
+clm3 = 1; 
+clm4 = 1;
+clm5 = 1; 
+clm6 = 1;
+clm7 = 1; 
+turn = 1 ;
 
-//first player will be the red player 
-const firstPlayer = player1
 
-const gameOver = false; 
 
-//the board will have 6 rows and 7 columns 
-let board;
-const rows = 6; 
-const columns = 7;
+function check(player) {
+    setTimeout (() => {
+        for (x = 1; i <= 7; x++) {
+            for (y = 1; y <= 3; y++) {
+                if (document.getElementById(`c${x}r${y}`).style.backgroundColor == `${player}` && document.getElementById(`c${x}r${y + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${x}r${y + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${x}r${y + 3}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()  
+                }
 
-// window.onload method gets executed after the web page is loaded including all the elements related to the DOM 
-window.onload = function() {
-    setGame()
+                
+            }
+        }
+
+
+
+        for (x = 1; x <= 6; x++) {
+            for (y = 1; y <= 4; y++) {
+                if (document.getElementById(`c${x}r${y}`).style.backgroundColor == `${player}` && document.getElementById(`c${x}r${y + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${x}r${y + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${x}r${y + 3}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()  
+                }
+
+                
+            }
+        }
+
+        for (x = 1; x <= 4; x++) {
+            for (y = 1; y <= 3; y++) {
+                if (document.getElementById(`c${x}r${y}`).style.backgroundColor == `${player}` && document.getElementById(`c${x}r${y + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${x}r${y + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${x}r${y + 3}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()  
+                }
+
+                
+            }
+        }
+
+        for (x = 1; x <= 4; x++) {
+            for (y = 6; y >= 4; y--) {
+                if (document.getElementById(`c${x}r${y}`).style.backgroundColor == `${player}` && document.getElementById(`c${x + 1}r${y - 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${x + 2}r${y - 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${x + 3}r${j - 3}`).style.backgroundColor == `${player}`) {alert(`${player} wins`)
+                    location.reload()  
+                }
+
+                
+            }
+        }
+    }, 200)
 }
 
-function setGame() {
-    board [];
+document.querySelectorAll(".column").forEach((e) => {
+    e.addEventListener("click", () => {
 
-    for (let r= 0; r < rows; r++){
-        let row = []; 
+        sum = eval(`clm${e.id}`)
+        eval(`clm${e.id}++`)
 
-        for (let )
-    }
-}
+
+        if (sum <= 6 && turn % 2 != 0) {
+            document.getElementById(`${e.id}r${sum}`).style.backgroundColor = "red"
+            turn++
+            check('red')
+            document.getElementById("whosturn").innerText = "Yellow's Turn"
+        }
+        
+        else if (sum <= 6 && turn % 2 == 0) {
+            document.getElementById(`${e.id}r${sum}`).style.backgroundColor = "yellow"
+            turn++
+            check('yellow')
+            document.getElementById("whosturn").innerText = "Red's Turn"
+
+        }
+
+    })
+})
