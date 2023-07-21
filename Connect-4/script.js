ea936bc95e892247521c7d422ef542149f07063f
Romy <romyjeanpierre@yahoo.com>
2023-07-21 11:33:49 -0400

    day 5

--- Connect-4/script.js
+++ Connect-4/script.js
@@ -1,13 +1,13 @@
-let RedP = "R";
-let YellowP = "Y";
+let RedP = "Red";
+let YellowP = "Yellow";
 let firstPlayer= RedP;
 
 let gameOver = false;
 let board;// there are 7 columns and 6 rows 
 
-let rows = 6;
-let columns = 7;
-let trackColumns = []; //this will allow the game to keep track of which row each column is at.
+let rows = 6; //vertical will be x
+let columns = 7;// horizontal will be z 
+let trackColumns = []; 
 
 
 //onload is most often used within the <body> element to execute a script once a web page has completely loaded all content (including images, script files, CSS files, etc.).//https://www.w3schools.com/jsref/event_onload.asp
@@ -17,38 +17,40 @@ window.onload = function() {
 
 function setGame() { //this function will populate the tile within the board
     board = [];//the board will correspond to the tiles in the html page
-    trackColumns = [5, 5, 5, 5, 5, 5, 5];
+    trackColumns = [5, 5, 5, 5, 5, 5, 5]; // for each column has 5 tiles -1 
 
     for (let x = 0; x < rows; x++) {
         let row = [];
         for (let z = 0; z < columns; z++) {
-            // JS
             row.push(' ');
-            // HTML
+            // In HTML
             let tile = document.createElement("div");
             tile.id = x.toString() + "-" + z.toString();//create an ID that correspond to the array in JS 
-            tile.classList.add("tile");
-            tile.addEventListener("click", setPiece);
-            document.getElementById("board").append(tile);
+            tile.classList.add("tile");//add a class for the tile 
+            tile.addEventListener("click", Piece);
+            document.getElementById("board").append(tile);/*append this tag in the board in HTML. this is similar to adding <div id = "" class = "title"></div> 42 times in HTML. however by appending it, js eliminate that process */
         }
-        board.push(row);
+        board.push(row);//this adds the above function to the JS board 
     }
 }
 
-function setPiece() {
+//define the function here 
+//If game is over we return do nothing 
+
+function Piece() {
     if (gameOver) {
         return;
     }
 
     //get coords of that tile clicked
-    let coords = this.id.split("-");
-    let x = parseInt(coords[0]);
-    let z = parseInt(coords[1]);
+    let area = this.id.split("-");/*The split() method takes a pattern and divides a String into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.*/
+    let x = parseInt(area[0]);
+    let z = parseInt(area[1]);//The parseInt() function parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems). https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
 
     // figure out which row the current column should be on
     x = trackColumns[z]; 
 
-    if (x < 0) { // board[x][z] != ' '
+    if (x < 0) { 
         return;
     }
 
@@ -66,52 +68,52 @@ function setPiece() {
     x -= 1; //update the row height for that column
     trackColumns[z] = x; //update the array
 
-    checkWinner();
+    whoIsTheWinner();
 }
 
-function checkWinner() {
-     // horizontal
+function whoIsTheWinner() {
+     // Rows/Horizontal - run from left to right  or right to left 
      for (let x = 0; x < rows; x++) {
          for (let z = 0; z < columns - 3; z++){
             if (board[x][z] != ' ') {
                 if (board[x][z] == board[x][x+1] && board[x][z+1] == board[x][z+2] && board[x][z+2] == board[x][z+3]) {
-                    setWinner(x, z);
+                    theWinner(x, z);
                     return;
                 }
             }
          }
     }
 
-    // vertical
+    // Columns/vertical - goes up and down
     for (let z = 0; z < columns; z++) {
         for (let x = 0; x < rows - 3; x++) {
             if (board[x][z] != ' ') {
                 if (board[x][z] == board[x+1][z] && board[x+1][z] == board[x+2][z] && board[x+2][z] == board[x+3][z]) {
-                    setWinner(x, z);
+                    theWinner(x, z);
                     return;
                 }
             }
         }
     }
 
-    // anti diagonal
+    // Inverse diagonal - all the entries are zero except those on the diagonal going from the lower left corner to the upper right corner (↗),
     for (let x = 0; x < rows - 3; x++) {
         for (let z = 0; z < columns - 3; z++) {
             if (board[x][z] != ' ') {
                 if (board[x][z] == board[x+1][z+1] && board[x+1][z+1] == board[x+2][z+2] && board[x+2][z+2] == board[x+3][z+3]) {
-                    setWinner(x, z);
+                    theWinner(x, z);
                     return;
                 }
             }
         }
     }
 
-    // diagonal
+    // diagonal - from one corner odf the board to the other 
     for (let x = 3; x < rows; x++) {
         for (let z = 0; z < columns - 3; z++) {
             if (board[x][z] != ' ') {
                 if (board[x][z] == board[x-1][z+1] && board[x-1][z+1] == board[x-2][z+2] && board[x-2][z+2] == board[x-3][z+3]) {
-                    setWinner(x, z);
+                    theWinner(x, z);
                     return;
                 }
             }
@@ -119,7 +121,7 @@ function checkWinner() {
     }
 }
 
-function setWinner(x, z) {
+function theWinner(x, z) {
     let winner = document.getElementById("winner");
     if (board[x][z] == RedP) {
         winner.innerText = "Way to Go Red!!!";             
